7f19404e529f1bb52996ed73d4d1de1779ccf62b
arti-max <128210100+arti-max@users.noreply.github.com>
2025-02-27 11:56:40 +0300

    Update memcpy.asm
    memcpy fixed

--- lib/memcpy.asm
+++ lib/memcpy.asm
@@ -2,16 +2,27 @@ global _memcpy
 
 ; Memory Copy
 ; stack - arg
+; (dest, src, count)
 _memcpy:
    push bp
    mov bp, sp
-   push ax
-   push bx
-   mov ax, [bp+2]
-   mov bx, [bp+4]
-   mov [bx], [ax]
-   pop bx
-   pop ax
+   push si
+   push di
+   push cx
+   mov di, [bp+4] ; dest
+   mov si, [bp+6] ; src
+   mov cx, [bp+8] ; count
+
+   cmp cx, 0
+   je .done
+
+   cld
+
+   rep movsb
+
+.done:
+   pop si
+   pop di
+   pop cx
    pop bp
-   mov sp, bp
    ret
